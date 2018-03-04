8a839b49c22c43f157e7b1b7e1bea4b06f475d41
Eric Griffith <EGriffith92@gmail.com>
2018-03-03 19:55:22 -0500

    add rpmfusion

--- rpmfusion/recipes/default.rb
+++ rpmfusion/recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+execute 'Install RPMFusion' do
+	command 'yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm'
+end

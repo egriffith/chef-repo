542c3b071b5d3f45637debe84c3926275a4235a9
Eric Griffith <EGriffith92@gmail.com>
2018-03-08 09:59:35 -0500

    Add create directory for .ssh

--- auth-key/recipes/default.rb
+++ auth-key/recipes/default.rb
@@ -4,6 +4,13 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+directory '/home/joker/.ssh' do
+  	owner 'joker'
+  	group 'joker'
+  	mode '0700'
+  	action :create
+end
+
 cookbook_file '/home/joker/.ssh/authorized_keys' do
 	source 'authorized_keys'
 	owner 'joker'

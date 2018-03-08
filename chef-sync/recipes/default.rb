8c321a6d838a5c7b02907e8de50b3b0a8a151d44
Eric Griffith <EGriffith92@gmail.com>
2018-03-08 10:56:42 -0500

    Add chef-sync. Make chef-repo-git-sync persistent

--- chef-sync/recipes/default.rb
+++ chef-sync/recipes/default.rb
@@ -3,3 +3,27 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+cookbook_file '/etc/systemd/system/chef-sync.timer' do
+	source 'chef-sync.timer'
+	owner 'root'
+	group 'root'
+	mode '0744'
+	action :create
+end
+
+cookbook_file '/etc/systemd/system/chef-sync.service' do
+	source 'chef-sync.service'
+	owner 'root'
+	group 'root'
+	mode '0744'
+	action :create
+end
+
+execute "Reloading systemd" do
+	command "systemctl daemon-reload"
+end
+
+service 'chef-sync.timer' do
+	action [:enable, :start]
+end

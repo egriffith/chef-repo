58351c7b1a0369a97b73984d57c12a241750e9b0
Eric Griffith <EGriffith92@gmail.com>
2018-03-08 13:04:57 -0500

    Set edi's tuned to network-latency

--- tuned-edi/recipes/default.rb
+++ tuned-edi/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'tuned'
+
+execute "Set tuned to network-latency mode" do
+	command "tuned-adm profile network-latency"
+end
+

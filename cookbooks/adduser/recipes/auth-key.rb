c31b365a464b268225fa1c07c2be35cb1ab2e4ee
Eric Griffith <EGriffith92@gmail.com>
2018-05-15 22:58:37 +0000

    Attempt 2 at avoiding conflicting quotes.

--- cookbooks/adduser/recipes/auth-key.rb
+++ cookbooks/adduser/recipes/auth-key.rb
@@ -1,14 +1,14 @@
 directory '/home/#{node["adduser"]["username"]}/.ssh' do
-  	owner '#{node['adduser']['username']'
-  	group '#{node['adduser']['username']'
+  	owner '#{node["adduser"]["username"]'
+  	group '#{node["adduser"]["username"]'
   	mode '0700'
   	action :create
 end
 
 remote_file '/home/#{node[:adduser][:username]}/.ssh/authorized_keys' do
 	source 'https://github.com/egriffith.keys'
-	owner '#{node['adduser']['username']'
-	group '#{node['adduser']['username']'
+	owner '#{node["adduser"]["username"]'
+	group '#{node["adduser"]["username"]'
 	mode '0700'
 	action :create
 end
